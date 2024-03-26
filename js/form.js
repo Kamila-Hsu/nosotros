241826937efc3fd78c007dbdfa387107939e27ce
Kamila-Hsu <157466229+Kamila-Hsu@users.noreply.github.com>
2024-03-26 16:33:23 +0800

    Add files via upload

---
+++ js/form.js
@@ -0,0 +1,9 @@
+
+
+
+// -----Country Code Selection
+$('#mobileCode').intlTelInput({
+	initialCountry: "in",
+	separateDialCode: true,
+	// utilsScript: "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/11.0.4/js/utils.js"
+});
